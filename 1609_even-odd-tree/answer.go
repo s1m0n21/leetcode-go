e50e7cd02dca7d1c9a986e8b539344c4f8cd5906
S1M0N21 <s1m0n21@hotmail.com>
2021-10-20 16:23:54 +0800

    Add: #1609(even-odd-tree)

---
+++ 1609_even-odd-tree/answer.go
@@ -0,0 +1,51 @@
+/**
+ * @Author         : s1m0n21
+ * @Description    : Answer for https://leetcode-cn.com/problems/even-odd-tree/
+ * @Project        : leetcode-go
+ * @File           : answer.go
+ * @Date           : 2021/10/20 3:41 下午
+ */
+
+package _even_odd_tree
+
+import (
+	"github.com/s1m0n21/leetcode-go/utils"
+)
+
+type elem struct {
+	node  *utils.TreeNode
+	level int
+}
+
+func isEvenOddTree(root *utils.TreeNode) bool {
+	isEven := func(n int) bool { return n%2 == 0 }
+	queue := []elem{{root, 0}}
+
+	var last *elem
+	for len(queue) > 0 {
+		e := queue[0]
+		queue = queue[1:]
+
+		if last != nil && last.level != e.level {
+			last = nil
+		}
+
+		if (isEven(e.level) && isEven(e.node.Val)) || (!isEven(e.level) && !isEven(e.node.Val)) {
+			return false
+		}
+		if last != nil && ((isEven(e.level) && last.node.Val >= e.node.Val) || (!isEven(e.level) && last.node.Val <= e.node.Val)) {
+			return false
+		}
+
+		if e.node.Left != nil {
+			queue = append(queue, elem{e.node.Left, e.level + 1})
+		}
+		if e.node.Right != nil {
+			queue = append(queue, elem{e.node.Right, e.level + 1})
+		}
+
+		last = &e
+	}
+
+	return true
+}
---
+++ 1609_even-odd-tree/answer_test.go
@@ -0,0 +1,33 @@
+/**
+ * @Author         : s1m0n21
+ * @Description    : Test answer
+ * @Project        : leetcode-go
+ * @File           : answer_test.go
+ * @Date           : 2021/10/20 4:13 下午
+ */
+
+package _even_odd_tree
+
+import (
+	"github.com/s1m0n21/leetcode-go/utils"
+	"testing"
+)
+
+func TestAnswer(t *testing.T) {
+	tests := []struct {
+		input  *utils.TreeNode
+		expect bool
+	}{
+		{utils.MakeTreeNode(1, 10, 4, 3, nil, 7, 9, 12, 8, 6, nil, nil, 2), true},
+		{utils.MakeTreeNode(5, 4, 2, 3, 3, 7), false},
+		{utils.MakeTreeNode(5, 9, 1, 3, 5, 7), false},
+		{utils.MakeTreeNode(11, 8, 6, 1, 3, 9, 11, 30, 20, 18, 16, 12, 10, 4, 2, 17), true},
+		{utils.MakeTreeNode(1), true},
+	}
+
+	for i, test := range tests {
+		if actual := isEvenOddTree(test.input); actual != test.expect {
+			t.Errorf("%d: input = %+v, expect = %t, actual = %t", i, test.input.Preorder(), test.expect, actual)
+		}
+	}
+}

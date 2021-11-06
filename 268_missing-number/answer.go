622c6cbf46332aa50782c7ccd3c52fb94e96905c
S1M0N21 <s1m0n21@hotmail.com>
2021-11-06 23:54:22 +0800

    Update: #268(missing-number)

--- 268_missing-number/answer.go
+++ 268_missing-number/answer.go
@@ -1,9 +1,22 @@
 /**
  * @Author         : s1m0n21
- * @Description    :
+ * @Description    : Answer for https://leetcode-cn.com/problems/missing-number/
  * @Project        : leetcode-go
  * @File           : answer.go
  * @Date           : 2021/11/6 11:37 下午
  */
 
-package _68_missing_number
+package _missing_number
+
+func missingNumber(nums []int) int {
+	ans := 0
+	for i := 0; i <= len(nums); i++ {
+		ans += i
+	}
+
+	for _, n := range nums {
+		ans -= n
+	}
+
+	return ans
+}
--- 268_missing-number/answer_test.go
+++ 268_missing-number/answer_test.go
@@ -1,9 +1,29 @@
 /**
  * @Author         : s1m0n21
- * @Description    : 
+ * @Description    : Test answer
  * @Project        : leetcode-go
  * @File           : answer_test.go
  * @Date           : 2021/11/6 11:41 下午
  */
 
 package _missing_number
+
+import "testing"
+
+func TestAnswer(t *testing.T) {
+	tests := []struct {
+		input  []int
+		expect int
+	}{
+		{[]int{3, 0, 1}, 2},
+		{[]int{0, 1}, 2},
+		{[]int{9, 6, 4, 2, 3, 5, 7, 0, 1}, 8},
+		{[]int{0}, 1},
+	}
+
+	for i, test := range tests {
+		if actual := missingNumber(test.input); actual != test.expect {
+			t.Errorf("%d: input = %+v, expect = %d, actual = %d", i, test.input, test.expect, actual)
+		}
+	}
+}

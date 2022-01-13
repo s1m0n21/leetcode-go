5f655ff7df5452acdd4e276b202329ccee0a0669
S1M0N21 <s1m0n21@hotmail.com>
2022-01-13 21:15:10 +0800

    Update: #350(intersection-of-two-arrays-ii)

--- 350_intersection-of-two-arrays-ii/answer.go
+++ 350_intersection-of-two-arrays-ii/answer.go
@@ -1,6 +1,6 @@
 /**
  * @Author         : s1m0n21
- * @Description    : Answer for https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
+ * @Description    : Answer of https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/
  * @Project        : leetcode-go
  * @File           : answer.go
  * @Date           : 2021/6/30 11:43 下午
@@ -8,20 +8,29 @@
 
 package _intersection_of_two_arrays_ii
 
+import "sort"
+
 func intersect(nums1 []int, nums2 []int) []int {
-	var appeared = make(map[int]int)
-	var out []int
+	var ans []int
+	var m, n = len(nums1), len(nums2)
 
-	for _, n := range nums1 {
-		appeared[n]++
-	}
+	sort.Ints(nums1)
+	sort.Ints(nums2)
 
-	for _, n := range nums2 {
-		if freq, has := appeared[n]; has && freq > 0 {
-			out = append(out, n)
-			appeared[n]--
+	var i, j int
+	for i < m && j < n {
+		if nums1[i] == nums2[j] {
+			ans = append(ans, nums1[i])
+			i++
+			j++
+		} else if nums1[i] < nums2[j] {
+			for ; i < m && nums1[i] < nums2[j]; i++ {
+			}
+		} else if nums2[j] < nums1[i] {
+			for ; j < n && nums2[j] < nums1[i]; j++ {
+			}
 		}
 	}
 
-	return out
+	return ans
 }
--- 350_intersection-of-two-arrays-ii/answer_test.go
+++ 350_intersection-of-two-arrays-ii/answer_test.go
@@ -8,11 +8,29 @@
 
 package _intersection_of_two_arrays_ii
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestAnswer(t *testing.T) {
-	n1 := []int{4,9,5}
-	n2 := []int{9,4,9,8,4}
+	type input struct {
+		nums1, nums2 []int
+	}
 
-	t.Logf("answer = %+v", intersect(n1, n2))
+	tests := []struct {
+		input  input
+		expect []int
+	}{
+		{input{[]int{1, 2, 2, 1}, []int{2, 2}}, []int{2, 2}},
+		{input{[]int{4, 9, 5}, []int{9, 4, 9, 8, 4}}, []int{4, 9}},
+		{input{[]int{1, 2}, []int{1, 1}}, []int{1}},
+		{input{[]int{0, 5, 8, 7, 2, 9, 7, 5}, []int{1, 4, 8, 9}}, []int{8, 9}},
+	}
+
+	for i, test := range tests {
+		if actual := intersect(test.input.nums1, test.input.nums2); !reflect.DeepEqual(actual, test.expect) {
+			t.Errorf("%d: input = %+v, expect = %+v, actual = %+v", i, test.input, test.expect, actual)
+		}
+	}
 }

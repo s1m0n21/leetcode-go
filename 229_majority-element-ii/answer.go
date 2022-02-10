568032791d8826b156071fea34499ee3410cef63
S1M0N21 <s1m0n21@hotmail.com>
2022-02-10 11:16:48 +0800

    Optimiz: #229(majority-element-ii)

--- 229_majority-element-ii/answer.go
+++ 229_majority-element-ii/answer.go
@@ -9,18 +9,42 @@
 package _majority_element_ii
 
 func majorityElement(nums []int) []int {
-	e := make(map[int]int)
+	var ans []int
+	var major1, major2 int
+	var vote1, vote2 int
+	var cnt1, cnt2 int
+
 	for _, n := range nums {
-		e[n]++
+		if vote1 > 0 && n == major1 {
+			vote1++
+		} else if vote2 > 0 && n == major2 {
+			vote2++
+		} else if vote1 == 0 {
+			major1 = n
+			vote1++
+		} else if vote2 == 0 {
+			major2 = n
+			vote2++
+		} else {
+			vote1--
+			vote2--
+		}
 	}
 
-	var ans []int
-	m := len(nums) / 3
 	for _, n := range nums {
-		if e[n] > m {
-			ans = append(ans, n)
-			delete(e, n)
+		if vote1 > 0 && n == major1 {
+			cnt1++
 		}
+		if vote2 > 0 && n == major2 {
+			cnt2++
+		}
+	}
+
+	if vote1 > 0 && cnt1 > len(nums)/3 {
+		ans = append(ans, major1)
+	}
+	if vote2 > 0 && cnt2 > len(nums)/3 {
+		ans = append(ans, major2)
 	}
 
 	return ans

ba56d010d5ebddd67afcceef2bda8bdf6f8783ac
S1M0N21 <s1m0n21@hotmail.com>
2022-02-11 09:46:30 +0800

    Optimize: #75(sort_colors)

--- 75_sort-colors/answer.go
+++ 75_sort-colors/answer.go
@@ -9,23 +9,16 @@
 package _sort_colors
 
 func sortColors(nums []int) {
-	k, j := -1, len(nums)
-	for i := 0; i < j; {
+	l, r := -1, len(nums)
+	for i := 0; i < r; {
 		if nums[i] == 0 {
-			k++
-			swap(nums, k, i)
+			l++
+			nums[l], nums[i] = nums[i], nums[l]
 		} else if nums[i] == 2 {
-			j--
-			swap(nums, i, j)
+			r--
+			nums[r], nums[i] = nums[i], nums[r]
 			continue
 		}
-
 		i++
 	}
-}
-
-func swap(nums []int, a, b int) {
-	n := nums[a]
-	nums[a] = nums[b]
-	nums[b] = n
 }

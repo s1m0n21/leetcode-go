2a338b30fa9d8d39c4db863c8ce9cbcecc98b837
S1M0N21 <s1m0n21@hotmail.com>
2022-02-17 19:57:06 +0800

    Optimiz: #415(add-strings)

--- 415_add-strings/answer.go
+++ 415_add-strings/answer.go
@@ -12,26 +12,19 @@ func addStrings(num1 string, num2 string) string {
 	var bytes []byte
 	var p1, p2, n = len(num1) - 1, len(num2) - 1, 0
 
-	for p1 >= 0 || p2 >= 0 {
-		var sum int
-		if p1 >= 0 && p2 >= 0 {
-			sum = int(num1[p1]-'0') + int(num2[p2]-'0') + n
+	for p1 >= 0 || p2 >= 0 || n > 0 {
+		var sum, x, y int
+		if p1 >= 0 {
+			x = int(num1[p1] - '0')
 			p1--
-			p2--
-		} else if p1 >= 0 {
-			sum = int(num1[p1]-'0') + n
-			p1--
-		} else {
-			sum = int(num2[p2]-'0') + n
+		}
+		if p2 >= 0 {
+			y = int(num2[p2] - '0')
 			p2--
 		}
+		sum = x + y + n
 		n = sum / 10
-		sum %= 10
-		bytes = append(bytes, byte(sum+'0'))
-	}
-
-	if n > 0 {
-		bytes = append(bytes, byte(n+'0'))
+		bytes = append(bytes, byte(sum%10+'0'))
 	}
 
 	for i := 0; i < len(bytes)/2; i++ {
---
+++ 43_multiply-strings/answer.go
@@ -0,0 +1,9 @@
+/**
+ * @Author         : s1m0n21
+ * @Description    : 
+ * @Project        : leetcode-go
+ * @File           : answer.go
+ * @Date           : 2022/2/17 7:42 PM
+ */
+
+package _3_multiply_strings

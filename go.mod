3ef12aabcb7d7b23e41cb7ab49102adef0d95cf3
s1m0n21 <s1m0n21@hotmail.com>
2022-03-21 18:07:05 +0800

    add test util

--- go.mod
+++ go.mod
@@ -1,16 +1,23 @@
 module github.com/s1m0n21/leetcode-go
 
-go 1.16
+go 1.18
 
 require (
 	github.com/awalterschulze/gographviz v2.0.3+incompatible
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/s1m0n21/exec-go v0.0.0-20200311082616-5b2524a30991
 	github.com/s1m0n21/go-log v1.0.0
+)
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
 	golang.org/x/lint v0.0.0-20200130185559-910be7a94367 // indirect
 	golang.org/x/tools v0.0.0-20200827010519-17fd2f27a9e3 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	honnef.co/go/tools v0.0.1-2020.1.3 // indirect
 )
--- utils/test.go
+++ utils/test.go
@@ -1,9 +1,67 @@
 /**
  * @Author         : s1m0n21
- * @Description    : 
+ * @Description    : Test util
  * @Project        : leetcode-go
  * @File           : test.go
  * @Date           : 2022/3/17 01:39
  */
 
 package utils
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+type ConfigOption func(config *testCaseConfig)
+
+type testCaseConfig struct{}
+
+type TestCase[I, E any] struct {
+	t      *testing.T
+	input  I
+	expect E
+	fn     func(I) E
+	config *testCaseConfig
+}
+
+func NewTestCase[I, E any, T TestCase[I, E]](t *testing.T, fn func(I) E, options ...ConfigOption) T {
+	config := &testCaseConfig{}
+	for _, op := range options {
+		op(config)
+	}
+
+	return T{fn: fn, config: config, t: t}
+}
+
+func (tc *TestCase[I, E]) SetInput(v I) {
+	tc.input = v
+}
+
+func (tc *TestCase[I, E]) GetInput() I {
+	return tc.input
+}
+
+func (tc *TestCase[I, E]) SetExpect(v E) {
+	tc.expect = v
+}
+
+func (tc *TestCase[I, E]) GetExpect() E {
+	return tc.expect
+}
+
+func (tc *TestCase[I, E]) RunTest() {
+	st := time.Now()
+	if actual := tc.fn(tc.input); !reflect.DeepEqual(actual, tc.expect) {
+		tc.t.Errorf("INPUT( %#v ) EXPECT( %#v ) ACTUAL( %#v ) --- FAIL", tc.input, tc.expect, actual)
+	} else {
+		tc.t.Logf("INPUT( %#v ) ELAPSED( %s ) --- PASS", tc.input, time.Now().Sub(st).String())
+	}
+}
+
+func (tc *TestCase[I, E]) SetAndRun(input I, expect E) {
+	tc.SetInput(input)
+	tc.SetExpect(expect)
+	tc.RunTest()
+}

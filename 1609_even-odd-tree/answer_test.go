a3776903a505a069e7bd125ab2c4858964f84794
s1m0n21 <s1m0n21@hotmail.com>
2022-03-22 17:27:37 +0800

    update test cases

--- 1609_even-odd-tree/answer_test.go
+++ 1609_even-odd-tree/answer_test.go
@@ -14,20 +14,11 @@ import (
 )
 
 func TestAnswer(t *testing.T) {
-	tests := []struct {
-		input  *utils.TreeNode
-		expect bool
-	}{
-		{utils.MakeBinaryTree(1, 10, 4, 3, nil, 7, 9, 12, 8, 6, nil, nil, 2), true},
-		{utils.MakeBinaryTree(5, 4, 2, 3, 3, 7), false},
-		{utils.MakeBinaryTree(5, 9, 1, 3, 5, 7), false},
-		{utils.MakeBinaryTree(11, 8, 6, 1, 3, 9, 11, 30, 20, 18, 16, 12, 10, 4, 2, 17), true},
-		{utils.MakeBinaryTree(1), true},
-	}
+	testCase := utils.NewTestCase(t, isEvenOddTree)
 
-	for i, test := range tests {
-		if actual := isEvenOddTree(test.input); actual != test.expect {
-			t.Errorf("%d: input = %+v, expect = %t, actual = %t", i, test.input.Preorder(), test.expect, actual)
-		}
-	}
+	testCase.SetAndRun(utils.MakeBinaryTree(1, 10, 4, 3, nil, 7, 9, 12, 8, 6, nil, nil, 2), true)
+	testCase.SetAndRun(utils.MakeBinaryTree(5, 4, 2, 3, 3, 7), false)
+	testCase.SetAndRun(utils.MakeBinaryTree(5, 9, 1, 3, 5, 7), false)
+	testCase.SetAndRun(utils.MakeBinaryTree(11, 8, 6, 1, 3, 9, 11, 30, 20, 18, 16, 12, 10, 4, 2, 17), true)
+	testCase.SetAndRun(utils.MakeBinaryTree(1), true)
 }

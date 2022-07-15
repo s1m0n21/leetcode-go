b4b4ee127b15eb18c77a8e2086bcf288c1614bd6
s1m0n21 <s1m0n21@hotmail.com>
2022-07-15 11:13:46 +0800

    optimized sulution

--- 121_best-time-to-buy-and-sell-stock/answer.go
+++ 121_best-time-to-buy-and-sell-stock/answer.go
@@ -9,19 +9,25 @@
 package _best_time_to_buy_and_sell_stock
 
 func maxProfit(prices []int) int {
-	var ans int
-	min := prices[0]
-
-	for i := 1; i < len(prices); i++ {
-		if prices[i] < min {
-			min = prices[i]
-			continue
-		}
-
-		if prices[i]-min > ans {
-			ans = prices[i] - min
-		}
+	minPrice, maxProfit := 10001, 0
+	for _, price := range prices {
+		maxProfit = max(maxProfit, price-minPrice)
+		minPrice = min(price, minPrice)
 	}
 
-	return ans
+	return maxProfit
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
